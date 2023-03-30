4a8b497d912ad9c28aefd20bba93630a83ef974f
Ismail el alaoui <isma2697@gmail.com>
2023-03-30 12:14:05 +0000

    imlementacion only book info

--- resources/views/components/mycomp/book-info.blade.php
+++ resources/views/components/mycomp/book-info.blade.php
@@ -4,12 +4,20 @@
     </div>
     <div class="book-info">
         <h2>{{ $book->title }}</h2>
-        <p>{{ $book->authors }}</p>
-        <p>{{ $book->subtitle }}</p>
-        <p>{{ $book->published_date }}</p>
-        <p>{{ $book->page_count }}</p>
-        <p>{{ $book->categories }}</p>
-        <p>{{ $book->description }}</p>
-        <p>{{ $book->identifier }}</p>
+        <hr>
+        <p class="info">Subtitulo:</p>
+        <p class="info_db">{{ $book->subtitle ?? 'No hay subtitulo' }}</p>
+        <p class="info">Descripción:</p>
+        <p class="info_db">{{ $book->description ?? 'No hay descripción' }}</p>
+        <p class="info">Categoria:</p>
+        <p class="info_db">{{ $book->categories ?? 'No hay categoria' }}</p>
+        <p class="info">Autores:</p>
+        <p class="info_db">{{ $book->authors ?? 'No hay autores' }}</p>
+        <p class="info">Fecha de publicacion:</p>
+        <p class="info_db">{{ $book->published_date ?? 'No hay fecha de publicacion' }}</p>
+        <p class="info">Números de paginas:</p>
+        <p class="info_db">{{ $book->page_count ?? 'No hay numero de paginas' }}</p>
+        <p class="info">Números de identificador:</p>
+        <p class="info_db">{{ $book->identifier ?? 'No hay numero de identificador' }}</p>
     </div>
 </div>
