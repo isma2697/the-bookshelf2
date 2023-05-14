8846ff88ffc786b0007574b7b2f0c55882018208
isma2697 <isma2697@gmail.com>
2023-05-14 16:48:59 +0100

    ·

--- resources/views/components/mycomp/floating-box.blade.php
+++ resources/views/components/mycomp/floating-box.blade.php
@@ -21,6 +21,7 @@
                     <a href="{{ route('profile.likes',['section' => 'likes']) }}" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
                         Mis libros
                     </a>
+                    
 
                     <a href="{{ route('profile.likes', ['section' => 'likes']) }}" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
                         Likes
